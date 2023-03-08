bf6a1af29aa24b2c5e9e1d67f7322a59ea3d974a
Aditya Mulgundkar <adityamulgundkar.nmims@gmail.com>
2023-03-08 18:28:26 +0530

    Added classifier

--- Classifier.py
+++ Classifier.py
@@ -7,7 +7,7 @@ import pandas as pd
 aX, aY = [], []
 Xdata, Ydata = np.asarray([]), np.asarray([])
 
-motor_num = 3
+motor_num = 1
 
 import pandas as pd
 # for k in range(1,6):
@@ -27,69 +27,54 @@ import pandas as pd
 for j in range(1,10):
     data = pd.read_csv(f"dist/hexa-x/m{motor_num}/test{j}.csv")
     for i in range(1,len(data.loc[:, "R"])):
-        # if(i < 3 or i > len(data.loc[:, "R"])-3):
-        #     ax = data.loc[:,"R"][i], data.loc[:,"R"][i], data.loc[:,"R"][i], data.loc[:,"R"][i], data.loc[:,"R"][i], data.loc[:,"RDes"][i], data.loc[:,"P"][i], data.loc[:,"P"][i], data.loc[:,"P"][i], data.loc[:,"P"][i], data.loc[:,"P"][i], data.loc[:,"PDes"][i], data.loc[:,"Y"][i], data.loc[:,"Y"][i], data.loc[:,"Y"][i], data.loc[:,"Y"][i], data.loc[:,"Y"][i], data.loc[:,"YDes"][i]
+        # if(i < 3 or i > len(data.loc[:, "R"])):
+        #     ax = data.loc[:,"R"][i], data.loc[:,"R"][i], data.loc[:,"R"][i], data.loc[:,"RDes"][i], data.loc[:,"P"][i], data.loc[:,"P"][i], data.loc[:,"P"][i], data.loc[:,"PDes"][i], data.loc[:,"Y"][i], data.loc[:,"Y"][i], data.loc[:,"Y"][i], data.loc[:,"YDes"][i]
         # else:
-        #     ax = data.loc[:,"R"][i-2], data.loc[:,"R"][i-1], data.loc[:,"R"][i], data.loc[:,"R"][i+1], data.loc[:,"R"][i+2], data.loc[:,"RDes"][i], data.loc[:,"P"][i-2], data.loc[:,"P"][i-1], data.loc[:,"P"][i], data.loc[:,"P"][i+1], data.loc[:,"P"][i+2], data.loc[:,"PDes"][i], data.loc[:,"Y"][i-2], data.loc[:,"Y"][i-1], data.loc[:,"Y"][i], data.loc[:,"Y"][i+1], data.loc[:,"Y"][i+2], data.loc[:,"YDes"][i]
+        #     ax = data.loc[:,"R"][i], data.loc[:,"R"][i-1], data.loc[:,"R"][i-2], data.loc[:,"RDes"][i], data.loc[:,"P"][i], data.loc[:,"P"][i-1],  data.loc[:,"P"][i-2], data.loc[:,"PDes"][i], data.loc[:,"Y"][i], data.loc[:,"Y"][i-1], data.loc[:,"Y"][i-2], data.loc[:,"YDes"][i]
         # ax = data.loc[:,"R"][i], data.loc[:,"RDes"][i]
-        ax = data.loc[:,"R"][i], data.loc[:,"RDes"][i], data.loc[:,"P"][i], data.loc[:,"PDes"][i], data.loc[:,"Y"][i], data.loc[:,"YDes"][i]
+        # ax = data.loc[:,"R"][i], data.loc[:,"RDes"][i], data.loc[:,"P"][i], data.loc[:,"PDes"][i], data.loc[:,"Y"][i], data.loc[:,"YDes"][i]
+        if i < 10 or i > len(data.loc[:, "R"]):
+            ax = (
+                data.loc[:, "R"][i],
+                *([data.loc[:, "R"][i]] * 3),
+                data.loc[:, "RDes"][i],
+                data.loc[:, "P"][i],
+                *([data.loc[:, "P"][i]] * 3),
+                data.loc[:, "PDes"][i],
+                data.loc[:, "Y"][i],
+                *([data.loc[:, "Y"][i]] * 3),
+                data.loc[:, "YDes"][i]
+            )
+        else:
+            ax = (
+                data.loc[:, "R"][i],
+                data.loc[:, "R"][i-1],
+                data.loc[:, "R"][i-2],
+                data.loc[:, "R"][i-3],
+                data.loc[:, "RDes"][i],
+                data.loc[:, "P"][i],
+                data.loc[:, "P"][i-1],
+                data.loc[:, "P"][i-2],
+                data.loc[:, "P"][i-3],
+                data.loc[:, "PDes"][i],
+                data.loc[:, "Y"][i],
+                data.loc[:, "Y"][i-1],
+                data.loc[:, "Y"][i-2],
+                data.loc[:, "Y"][i-3],
+                data.loc[:, "YDes"][i]
+                )
         ay = data.loc[:,"FaultIn"][i]
         aX.append(ax)
-        aY.append(ay)    
-        
+        aY.append(ay) 
+
+    
 Xdata, Ydata = np.asarray(aX).reshape(len(aX),len(ax)), np.asarray(aY).reshape(len(aY),1)
 
-# Xdata, Ydata = generate_data(3000)
-print(f"Load x: {Xdata.shape}")
-print(f"Load y: {Ydata.shape}")
-# xtr, xte, ytr, yte = train_test_split(Xdata, Ydata, test_size=0.3)
 xtr = Xdata
 ytr = Ydata
 
-aX_te, aY_te = [], []
-Xdata_te, Ydata_te = np.asarray([]), np.asarray([])
-data = pd.read_csv(f"dist/hexa-x/m{motor_num}/real-test.csv")
-for i in range(1,len(data.loc[:, "R"])):
-    # ax = data.loc[:,"R"][i], data.loc[:,"RDes"][i], data.loc[:,"P"][i], data.loc[:,"PDes"][i], data.loc[:,"Y"][i], data.loc[:,"YDes"][i]
-    # ax = data.loc[:,"R"][i], data.loc[:,"RDes"][i]
-    ax = data.loc[:,"R"][i], data.loc[:,"RDes"][i], data.loc[:,"P"][i], data.loc[:,"PDes"][i], data.loc[:,"Y"][i], data.loc[:,"YDes"][i]
-    ay = data.loc[:,"FaultIn"][i]
-    aX_te.append(ax)
-    aY_te.append(ay)
-
-xte, yte = np.asarray(aX_te).reshape(len(aX_te),len(ax)), np.asarray(aY_te).reshape(len(aY_te),1)
-
-# print(f"xte: {(xte)}")
-# print(f"yte: {(yte)}")
-
-Rotate = RotationForest(n_trees=200, n_features=3)
-Random = RandomForestClassifier(n_estimators=200)
-Linear = LogisticRegression()
-
+Rotate = RotationForest(n_trees=1000, n_features=3)
 Rotate.fit(xtr, ytr)
-Random.fit(xtr, ytr)
-Linear.fit(xtr, ytr)
 
-preds_rotate = Rotate.predict(xte)
-preds_random = Random.predict(xte)
-preds_linear = Linear.predict(xte)
-obs = yte
-
-print("Validating...")
-
-df = pd.DataFrame(data=[preds_rotate, preds_random, obs, preds_linear]).T
-df['rotate'] = df[2].eq(df[0]).astype(int)
-df['random'] = df[2].eq(df[1]).astype(int)
-df['linear'] = df[2].eq(df[3]).astype(int)
-
-print(f"Accuracy rotate: {df['rotate'].sum() / len(df) * 100}%")
-print(f"Accuracy random: {df['random'].sum() / len(df) * 100}%")
-print(f"Accuracy linear: {df['linear'].sum() / len(df) * 100}%")
-
-import matplotlib.pyplot as plt
-
-# plt.plot(xte)
-plt.plot(yte)
-plt.plot(preds_rotate, linestyle='dotted')
-plt.axvline(x = 552, color = 'r', label = 'axvline - full height', linestyle='dotted')
-plt.show()
+import pickle
+pickle.dump(Rotate, open("models/rfc-1", 'wb'))
