f73d0ae11270d1b2edb284a7041702ad97fa81cb
Aditya Mulgundkar <adi@svoot.com>
2023-04-10 00:02:41 +0530

    Cleaned real validator; added pre-built pymavlink zip containing custom packet DESIRED_VELOCITY_RATES

--- Validator-M12-real.py
+++ Validator-M12-real.py
@@ -1,9 +1,5 @@
 import pickle
 from RotationForest import *
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
-
 import time
 from pymavlink import mavutil
 import numpy as np
@@ -16,51 +12,30 @@ def warn(*args, **kwargs):
 warnings.warn = warn
 warnings.filterwarnings("ignore")
 
-
 timestamp = time.strftime("%Y%m%d-%H%M%S")
 filename = f"dist/real-logs/log_{timestamp}.csv"
 
 log_writer = 0
 last_time = 0
+R, R_1, R_d = 0, 0, 0
 
 def log_this(time, value):
     log_writer.writerow([time, value])
     
 with open(filename, "a", newline='') as csvfile:
     log_writer = csv.writer(csvfile)
-
-    aX, aY = [], []
-    Xdata, Ydata = np.asarray([]), np.asarray([])
-
-    motor_num = 1
-
-    aX_te, aY_te = [], []
-    Xdata_te, Ydata_te = np.asarray([]), np.asarray([])
-
     Rotate = pickle.load(open("models/rfc-M12", 'rb'))
 
     mavutil.set_dialect("common")
-
     master = mavutil.mavlink_connection('udpin:0.0.0.0:14554')
     master.wait_heartbeat()
 
-    firstFlag = True
-    R, R_1, R_d = 0, 0, 0
     while True:
-        if firstFlag:
-            R, R_1, R_d = 0, 0, 0
-            firstFlag = False
-
         try:
             msg = master.recv_match(blocking=False, timeout=1)
             if not msg:
                 continue
             type = msg.get_type()
-            
-            if type == 'HEARTBEAT':
-                # print(f"{msg}")
-                last_time = msg.time_boot_ms
-            
             if type == 'UNKNOWN_12921' or type == 'DESIRED_VELOCITY_RATES':
                 # print(f"{msg}")
                 R_d = msg.rdes
